875e6eafc7a6f5e90f0d284372e653a0a27b291d
Sean Pimentel <sean_pimentel@hotmail.com>
2018-05-15 21:50:25 -0700

    Got the edit page done. Needs to be hooked up
    Signed-off-by: Sean Pimentel <sean_pimentel@hotmail.com>

--- mysite/portfolio/urls.py
+++ mysite/portfolio/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-    url(r'new_profile1', views.new_profile1, name='new_profile1')
+    url(r'new_profile1', views.new_profile1, name='new_profile1'),
+    url(r'post_edit', views.post_edit, name='post_edit')
 ]
