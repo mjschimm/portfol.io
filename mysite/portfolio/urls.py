ffaae6e5d2bb996d950f6a007326a3f00e623636
Sean Pimentel <sean_pimentel@hotmail.com>
2018-05-16 17:40:30 -0700

    Done with project view.
    Signed-off-by: Sean Pimentel <sean_pimentel@hotmail.com>

--- mysite/portfolio/urls.py
+++ mysite/portfolio/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-    url(r'new_profile1', views.new_profile1, name='new_profile1')
+    url(r'new_profile1', views.new_profile1, name='new_profile1'),
     url(r'post_view', views.post_view, name='post_view')
 ]
